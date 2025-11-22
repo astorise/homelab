6287926a0782089dfd1003673895474dbdde8021
astorise <astorise@gmail.com>
2025-11-22 21:12:30 +0100

    Modification permissions

--- home-http/src/main.rs
+++ home-http/src/main.rs
@@ -880,7 +880,7 @@ fn main() -> Result<()> {
 }
 
 fn named_pipe_stream() -> io::Result<UnboundedReceiverStream<Result<PipeConnection, io::Error>>> {
-    let sddl = "D:(A;;GA;;;SY)(A;;GA;;;BA)(A;;GA;;;AU)"; // Allow System, Built-in Admins, Authenticated Users
+    let sddl = "D:(A;;FA;;;SY)(A;;FA;;;BA)(A;;FA;;;AU)"; // Allow System, Built-in Admins, Authenticated Users
     let mut sd: windows_sys::Win32::Security::PSECURITY_DESCRIPTOR = std::ptr::null_mut();
     let sddl_w: Vec<u16> = sddl.encode_utf16().chain(std::iter::once(0)).collect();
 
